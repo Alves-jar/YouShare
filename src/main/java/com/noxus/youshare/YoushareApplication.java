5afcbfb40f132a64c9fedcc24d765b2a637c937a
AlvesKZ <igoralvex01@gmail.com>
2026-03-12 15:05:15 -0300

    create project

---
+++ src/main/java/com/noxus/youshare/YoushareApplication.java
@@ -0,0 +1,13 @@
+package com.noxus.youshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoushareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoushareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/noxus/youshare/YoushareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noxus.youshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoushareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
